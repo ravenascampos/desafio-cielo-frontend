9ade4722cf604badb1acf012b5245086f6ec1e26
ravena <ravenascampos@gmail.com>
2023-09-28 19:14:25 -0300

    feat: charts

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
+import { Box } from "@mui/material";
+import { PieChartDashboard } from "./components/PieChart";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Box>
+      <PieChartDashboard
+        title='Tipos de pagamentos'
+      />
+      <PieChartDashboard
+      title='Canal'
+      />
+    </Box>
   )
 }
