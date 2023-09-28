1df8360578d9505ea6b6ee027d58bae5611fbdaf
ravena <ravenascampos@gmail.com>
2023-09-28 14:23:40 -0300

    feat: menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,40 @@
-import { AppBar, Avatar, Grid, IconButton } from "@mui/material";
+import { AppBar, Avatar, Box, Drawer, Grid, IconButton, Toolbar, useMediaQuery, useTheme } from "@mui/material";
 import logo from '../../../images/logo.png';
 import avatar from '../../../images/avatar.jpg'
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
+import Menu from "../Menu";
 
 export default function Header() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleMenuToggle = () => {
+    setMobileOpen(!mobileOpen);
+  }
+
   return (
-    <AppBar position='absolute'>
-      <Grid
-        display='flex'
-        flexDirection='row'
-        justifyContent='space-between'
-        mr={2}
-        ml={2}
-      >
+    <Box>
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Grid
+          display='flex'
+          flexDirection='row'
+          justifyContent='space-between'
+          mr={2}
+          ml={isMobile ? 0 : 2}
+        >
         <Grid>
-          <img src={logo} width='50px'/>
+          { isMobile ?
+           ( <Toolbar>
+              <IconButton
+                onClick={handleMenuToggle}
+              >
+                <MenuIcon/>
+              </IconButton>
+            </Toolbar>) :
+            (<img src={logo} width='50px'/>)
+          }
         </Grid>
         <Grid
           display='flex'
@@ -29,7 +49,30 @@ export default function Header() {
             <Avatar src={avatar}/>
           </IconButton>
         </Grid>
-      </Grid>
-    </AppBar>
+        </Grid>
+      </AppBar>
+      <Box component='nav'>
+          <Drawer
+            variant='temporary'
+            open={mobileOpen}
+            onClose={handleMenuToggle}
+            sx={{
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', marginTop: '56px' },
+            }}
+          >
+            <Menu/>
+          </Drawer>
+          <Drawer
+            variant='permanent'
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', marginTop: '60px' },
+            }}
+          >
+            <Menu/>
+          </Drawer>
+      </Box>
+    </Box>
+    
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
+import Dashboard from '@/pages/Dashboard';
 import DetailsPage from '@/pages/DetailsPage';
 import ErrorPage from '@/pages/ErrorPage';
 import Home from '@/pages/Home';
+import Transactions from '@/pages/Transactions';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
@@ -10,7 +12,15 @@ export const router = createBrowserRouter([
     errorElement: <ErrorPage/>
   },
   {
+    path: '/transactions',
+    element: <Transactions/>
+  },
+  {
     path: '/transactions/:id',
     element: <DetailsPage/>
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard/>
   }
 ])
